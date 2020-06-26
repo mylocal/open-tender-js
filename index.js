b11a2b25f999ae8ed6f0a1c7f7934f1b6cbbd29d
JC Harrington <jc@brandibble.co>
2020-06-26 16:46:58 -0500

    Add files, set up initial exports

---
+++ index.js
@@ -0,0 +1,10 @@
+import * as constants from "./constants";
+
+// module.exports.orderTypeNamesMap = constants.orderTypeNamesMap;
+module.exports = {
+  orderTypeNamesMap: constants.orderTypeNamesMap,
+  otherOrderTypesMap: constants.otherOrderTypesMap,
+  tenderTypeNamesMap: constants.tenderTypeNamesMap,
+  loyaltyType: constants.loyaltyType,
+  MAX_DISTANCE: constants.MAX_DISTANCE,
+};
