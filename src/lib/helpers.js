f9ac429226cffaf7fe998cb064ad5a7e3bd95ff0
JC Harrington <jc@brandibble.co>
2021-03-21 22:07:25 -0500

    Add getWidth and getHeight functions

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -60,3 +60,23 @@ export const cleanPhone = (phone) => {
 
 export const makeRandomNumberString = () =>
   Math.floor(Math.random() * 1000000000).toString()
+
+export const getWidth = () => {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  )
+}
+
+export const getHeight = () => {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  )
+}
