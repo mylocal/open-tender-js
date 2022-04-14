7245c787dcf894fa8b53015a91b0c5f6913e6299
JC Harrington <jc@brandibble.co>
2022-04-13 22:38:58 -0400

    Add makeStatus and makeProgress loyalty functions

--- src/lib/rewards.js
+++ src/lib/rewards.js
@@ -1,3 +1,5 @@
+import { formatDollars, formatQuantity } from './cart'
+import { loyaltyType } from './constants'
 import { getMinutesfromDate, time24ToMinutes } from './datetimes'
 
 export const makeValidDeals = (
@@ -35,4 +37,40 @@ export const makeValidDeals = (
     return validDayparts.length > 0
   })
   return deals
+}
+
+export const makeStatus = (tiers, status, points) => {
+  if (!tiers) return null
+  const highest = tiers[tiers.length - 1].threshold
+  const total = highest * 1.2
+  const progress = Math.min((status.progress / total) * 100, 100)
+  const progressTiers = tiers.map((i) => ({
+    ...i,
+    points,
+    percentage: (i.threshold / total) * 100,
+    color: `#${i.hex_code}`,
+    value: !points
+      ? `${formatDollars(i.threshold, '', 0)}`
+      : `${formatQuantity(i.threshold)}`,
+  }))
+  const daysMsg =
+    status.days === 7300 ? 'all-time' : `in the last ${status.days} days`
+  const progressAmt = !points
+    ? formatDollars(status.progress, '', 0)
+    : formatQuantity(status.progress)
+  const progressMsg = !points
+    ? `${progressAmt} spent ${daysMsg}`
+    : `${progressAmt} ${points.name.toLowerCase()} earned ${daysMsg}`
+  return { progress, progressMsg, tiers: progressTiers }
+}
+
+export const makeProgress = (loyalty_type, spend, redemption) => {
+  if (!spend || !redemption || !loyalty_type) return null
+  const currentSpend = parseFloat(spend.current)
+  const threshold = parseFloat(redemption.threshold)
+  const progress =
+    loyalty_type === loyaltyType.CREDIT
+      ? parseInt((currentSpend / threshold) * 100)
+      : null
+  return progress
 }
