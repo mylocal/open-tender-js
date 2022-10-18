a0359131e9e619a6244624cc0e3f534119a5b370
JC Harrington <jc@brandibble.co>
2022-10-18 09:43:05 -0500

    Accommodate QR codes in prepareOrder

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -933,6 +933,7 @@ export const prepareOrder = (data) => {
   if (data.surcharges) order.surcharges = data.surcharges
   if (data.discounts) order.discounts = data.discounts
   if (data.promoCodes) order.promo_codes = data.promoCodes
+  if (data.qrCodes) order.qr_codes = data.qrCodes
   if (data.points) order.points = data.points
   if (data.tip) order.tip = data.tip
   if (data.tenders) order.tenders = data.tenders
